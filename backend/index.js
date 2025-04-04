077f5626491a88d856648d82a9cce3a68dfe37dd
Thejas K <143934668+thejas-1999@users.noreply.github.com>
2025-04-04 12:34:59 +0530

    connected to mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 8000;
+const mongoUrl = process.env.MONGO_URL;
 
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
-app.listen(port, () => {
-  console.log(`server is running on http://localhost:${port}`);
-});
+mongoose
+  .connect(mongoUrl)
+  .then(() => {
+    console.log(`server is connected to mongodb`);
+    app.listen(port, () => {
+      console.log(`server is running on http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`failed to connect : ${err}`);
+  });
